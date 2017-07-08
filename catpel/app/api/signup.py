2e5c4d044ffe28fe5c63edcabe260dcd96b4b4e5
Humbertzhang <504490160@qq.com>
2017-07-09 00:20:51 +0800

    server support

---
+++ catpel/app/api/signup.py
@@ -0,0 +1,27 @@
+from . import api
+from .. import db
+from flask import request
+from flask import jsonify
+from ..models import User
+
+@api.route('/signup/', methods=['POST'])
+def signup():
+    if request.method == 'POST':
+
+        un = request.get_json().get('username')
+        passwd = request.get_json().get('password')
+        
+        user = User.query.filter_by(username=un).first()
+
+        if user:
+            return jsonify({}), 400
+    
+        new_user = User(
+                        username=un,
+                        password=passwd,
+                        time=0
+                       )
+        
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'created': new_user.id}), 200
